4071821aba3d2a8c500938213d43ee067d41202e
Josh <jpostman314@yahoo.com>
2022-12-10 18:53:31 -0500

    Created the Functionality to Update a Specific Product in the Products App Views File

--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.products_list, name = 'products_list'),
+    path('', views.products_list),
+    path('<int:pk>/', views.product_detail),
 ]
--- products/views.py
+++ products/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Product
@@ -21,4 +22,15 @@ def products_list(request):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     
     
+@api_view(['GET', 'PUT'])
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    if request.method == 'GET':
+        serializer = ProductSerializer(product);
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = ProductSerializer(product, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
 
